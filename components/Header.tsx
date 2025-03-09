daeb594963076c43f781e203df9e180850aaa2ca
Sedat Uygur <sedat.uygur@outlook.com>
2025-03-09 19:12:30 +0300

    🎉 feat : create a footer component

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,28 +6,30 @@ import { SearchBar } from "@/components/ui/SearchBar";
 
 export function Header() {
   return (
-    <div className="hidden h-full flex-row md:flex">
-      <div className="container flex flex-row items-start justify-between space-y-2 p-4 sm:flex-row sm:items-center sm:space-y-0 md:h-20">
-        <div className="container flex flex-row items-start space-y-2 space-x-4">
-          <Link href="/">
-            <div className="container flex flex-row items-center space-x-2">
-              <Avatar>
-                <AvatarImage
-                  src="https://avatars.githubusercontent.com/u/90511329"
-                  alt="Sedat Can Uygur"
-                />
-                <AvatarFallback>SCU</AvatarFallback>
-              </Avatar>
-              <h2>sedatcanuygur.com</h2>
-            </div>
-          </Link>
-          <Link href="/blog">Blog</Link>
-        </div>
-        <div className="container flex flex-row items-start justify-end space-y-2 space-x-4 sm:flex-row sm:items-center sm:space-y-0 md:h-16">
-          <SearchBar />
-          <ModeToggle />
+    <header>
+      <div className="hidden h-full flex-row md:flex">
+        <div className="container flex flex-row items-start justify-between space-y-2 p-4 sm:flex-row sm:items-center sm:space-y-0 md:h-20">
+          <div className="container flex flex-row items-start space-y-2 space-x-4">
+            <Link href="/">
+              <div className="container flex flex-row items-center space-x-2">
+                <Avatar>
+                  <AvatarImage
+                    src="https://avatars.githubusercontent.com/u/90511329"
+                    alt="Sedat Can Uygur"
+                  />
+                  <AvatarFallback>SCU</AvatarFallback>
+                </Avatar>
+                <h2>sedatcanuygur.com</h2>
+              </div>
+            </Link>
+            <Link href="/blog">Blog</Link>
+          </div>
+          <div className="container flex flex-row items-start justify-end space-y-2 space-x-4 sm:flex-row sm:items-center sm:space-y-0 md:h-16">
+            <SearchBar />
+            <ModeToggle />
+          </div>
         </div>
       </div>
-    </div>
+    </header>
   );
 }
