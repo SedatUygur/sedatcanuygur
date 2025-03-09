1463779819444613cf01e96df389dfa61b1606ed
Sedat Uygur <sedat.uygur@outlook.com>
2025-03-09 22:56:26 +0300

    🎉 feat : add blog card skeleton to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,49 @@
-import { Progress } from "@/components/ui/Progress";
+import { Suspense } from "react";
+
+import { BlogCardSkeleton } from "@/components/skeletons/BlogCardSkeleton";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center space-y-2 py-4">
-      <h3>This site is under construction</h3>
-      <p>
-        I have never used Radix UI before so I am gonna learn how to style stuff
-      </p>
-      <Progress value={50} className="w-[60%]" />
+    <div className="flex flex-col min-h-screen">
+      <main className="flex-1">
+        <section className="w-full py-12 md:py-24 lg:py-32">
+          <div className="container px-4 md:px-6">
+            <div className="flex flex-col items-center space-y-4 text-center">
+              <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl lg:text-6xl">
+                Sedat Can Uygur - Software Engineer
+              </h1>
+              <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl">
+                Welcome to my personal website. Here you can find my latest blog
+                posts and projects.
+              </p>
+            </div>
+          </div>
+        </section>
+        <section className="w-full py-12 md:py-24 lg:py-32 bg-gray-100 dark:text-white dark:bg-slate-950">
+          <div className="container px-4 md:px-6">
+            <h2 className="text-3xl font-bold tracking-tighter text-center sm:text-4xl">
+              Latest Blog Posts
+            </h2>
+            <div className="mt-8 space-y-6">
+              <Suspense
+                fallback={
+                  <div className="flex items-center">
+                    <BlogCardSkeleton />
+                    <BlogCardSkeleton />
+                    <BlogCardSkeleton />
+                  </div>
+                }
+              >
+                <div className="flex flex-col items-center space-y-4">
+                  <BlogCardSkeleton />
+                  <BlogCardSkeleton />
+                  <BlogCardSkeleton />
+                </div>
+              </Suspense>
+            </div>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
