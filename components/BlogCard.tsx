b8b0e368cbff86dba09f38469935782f765dd426
Sedat Uygur <sedat.uygur@outlook.com>
2025-03-12 15:22:27 +0300

    🎉 feat : fix the blog card & add extra jest config

--- components/BlogCard.tsx
+++ components/BlogCard.tsx
@@ -2,33 +2,24 @@ import Image from "next/image";
 import Link from "next/link";
 
 export function BlogCard() {
-  <article className="p-4 bg-white rounded shadow">
-    {/* <img
-      alt="Post Image 5"
-      className="w-full h-auto rounded-md"
-      height="200"
-      src="/placeholder.svg"
-      style={{
-        aspectRatio: "300/200",
-        objectFit: "cover",
-      }}
-      width="300"
-    /> */}
-    <Image
-      alt="Post Image"
-      className="w-full h-auto rounded-md"
-      height={200}
-      src="/placeholder.svg"
-      style={{
-        aspectRatio: "300/200",
-        objectFit: "cover",
-      }}
-      width={300}
-    />
-    <h3 className="text-xl font-bold mt-4">Post Title</h3>
-    <p className="text-gray-500">Post Description...</p>
-    <Link className="text-blue-500 hover:underline" href="#">
-      Read More
-    </Link>
-  </article>;
+  return (
+    <article className="p-4 bg-white rounded shadow">
+      <Image
+        alt="Post Image"
+        className="w-full h-auto rounded-md"
+        height={200}
+        src="/placeholder.svg"
+        style={{
+          aspectRatio: "300/200",
+          objectFit: "cover",
+        }}
+        width={300}
+      />
+      <h3 className="text-xl font-bold mt-4">Post Title</h3>
+      <p className="text-gray-500">Post Description...</p>
+      <Link className="text-blue-500 hover:underline" href="#">
+        Read More
+      </Link>
+    </article>
+  );
 }
--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,7 @@
  */
 
 import type { Config } from "jest";
+import { TextDecoder, TextEncoder } from "util";
 
 const config: Config = {
   // All imported modules in your tests should be mocked automatically
@@ -67,7 +68,10 @@ const config: Config = {
   // globalTeardown: undefined,
 
   // A set of global variables that need to be available in all test environments
-  // globals: {},
+  globals: {
+    TextDecoder: TextDecoder,
+    TextEncoder: TextEncoder,
+  },
 
   // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
   // maxWorkers: "50%",
@@ -90,7 +94,9 @@ const config: Config = {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1",
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
@@ -137,7 +143,7 @@ const config: Config = {
   // setupFiles: [],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  // setupFilesAfterEnv: [],
+  setupFilesAfterEnv: ["<rootDir>/setup-env-vars.js"],
 
   // The number of seconds after which a test is considered as slow and reported as such in the results.
   // slowTestThreshold: 5,
