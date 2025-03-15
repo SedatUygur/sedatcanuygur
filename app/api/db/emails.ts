5b3c6a2af2992bb4e6e2d1624f6cdfe8ba668868
Sedat Uygur <sedat.uygur@outlook.com>
2025-03-15 21:51:40 +0300

    🎉 feat : inserts a new email into the emails table

---
+++ app/api/db/emails.ts
@@ -0,0 +1,36 @@
+"use server";
+
+import { Email, emails } from "@/db/schema";
+import { db } from "@/db";
+import { ParseDatabaseErrorResult } from "@/lib/db/ParseError";
+
+type InsertEmailProps = Pick<Email, "emailAddress" | "fullName" | "message">;
+
+type InsertEmailResponse = {
+  errorMessage?: string;
+  columnErrors?: ParseDatabaseErrorResult<InsertEmailProps>;
+};
+
+/**
+ * Inserts a new email into the emails table.
+ *
+ * @param email - The email details to insert.
+ *
+ * @returns An object with an errorMessage property if there was an error,
+ *   or an object with a columnErrors property if there were error(s) with
+ *   specific columns.
+ */
+export async function insertEmail(
+  email: InsertEmailProps,
+): Promise<InsertEmailResponse> {
+  try {
+    await db.insert(emails).values(email);
+    return {};
+  } catch (error) {
+    if (error) {
+      throw error;
+    } else {
+      return { errorMessage: "Unknown Error" };
+    }
+  }
+}
