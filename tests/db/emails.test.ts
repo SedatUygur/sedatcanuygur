308ad099b5774d85e091ebc4f5fa8999e9b26a97
Sedat Uygur <sedat.uygur@outlook.com>
2025-03-15 23:54:47 +0300

    🎉 fix : fix testing of the email insertion

--- tests/db/emails.test.ts
+++ tests/db/emails.test.ts
@@ -3,42 +3,49 @@ import { db } from "@/db";
 
 jest.mock("@/db", () => ({
   db: {
-    insert: {
-      values: jest.fn(),
-    },
-  },
-}));
-
-describe("insertEmail", () => {
-  // Skipping for now. I think I need to look into how to set up a mock database which isn't
-  // really something I'm interested in doing at this moment
-  //
-  // describe("when the data is invalid", () => {
-  //   it("returns a object with an error", async () => {
-  //     const response = await insertEmail({
-  //       fullName: "",
-  //       emailAddress: "not-an-email.com",
-  //       message: "Test Message"
-  //     });
-  //
-  //     expect(response).toMatchObject({
-  //       base: [],
-  //       formFields: {
-  //         fullName: ""
-  //       }
-  //     })
-  //   })
-  // })
-
-  describe("when the email is inserted", () => {
-    it("returns an empty object", async () => {
-      // Unsure of how to mock nested functions at the moment
-      // @ts-ignore
-      (db.insert.values as jest.Mock).mockReturnValueOnce({
+    insert: jest.fn().mockReturnValue({
+      values: jest.fn().mockReturnValue({
         data: {
           id: "testemailsuccess",
         },
         error: null,
+      }),
+    }),
+  },
+}));
+
+describe("insertEmail", () => {
+  /* Skipping for now. I think I need to look into how to set up a mock database which isn't
+  really something I'm interested in doing at this moment
+  
+  describe("when the data is invalid", () => {
+    it("returns a object with an error", async () => {
+      const response = await insertEmail({
+        fullName: "",
+        emailAddress: "not-an-email.com",
+        message: "Test Message"
+      });
+  
+      expect(response).toMatchObject({
+        base: [],
+        formFields: {
+          fullName: ""
+        }
+      })
+    })
+  }) */
+
+  describe("when the email is inserted", () => {
+    it("returns an empty object", async () => {
+      (db.insert as jest.Mock).mockImplementationOnce(() => {
+        return {
+          values: jest.fn().mockReturnValueOnce({
+            data: {
+              id: "testemailsuccess",
+            },
+            error: null,
+          }),
+        };
       });
 
       const response = await insertEmail({
