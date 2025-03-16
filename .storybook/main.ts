995a956aac746b62966a7dab126da9b117d46f8b
Sedat Uygur <sedat.uygur@outlook.com>
2025-03-16 17:34:34 +0300

    🎉 feat : remove unnecessary stories & write custom button story

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -8,8 +8,6 @@ const config: StorybookConfig = {
   ],
   addons: [
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-links",
     "@storybook/addon-onboarding",
     "@chromatic-com/storybook",
     "@storybook/experimental-addon-test",
