09da3532f5063df7a0cd85f502b275174870c8ea
Sedat Uygur <sedat.uygur@outlook.com>
2025-03-16 16:19:17 +0300

    🎉 feat : update tailwind config

--- ‎tailwind.config.ts
+++ ‎tailwind.config.ts
@@ -12,9 +12,12 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        warning: "hsl(var(--warning))",
+        "warning-foreground": "hsl(var(--warning-foreground))",
+      },
     },
   },
-  darkMode: "class",
   plugins: [],
 };
 export default config;
