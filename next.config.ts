50d615f7699654fef295d506556578b34dbc6eb2
Sedat Uygur <sedat.uygur@outlook.com>
2025-03-18 01:19:20 +0300

    🎉 feat : install @next/mdx and remark plugins to render MDX with Next.js

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from "next";
+import createMDX from "@next/mdx";
+import remarkFrontmatter from "remark-frontmatter";
+import remarkMdxFrontmatter from "remark-mdx-frontmatter";
+import remarkGfm from "remark-gfm";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  options: {
+    remarkPlugins: [
+      remarkGfm,
+      remarkFrontmatter,
+      [remarkMdxFrontmatter, { name: "frontmatter" }],
+    ],
+    rehypePlugins: [],
+  },
+});
+
+//export default nextConfig;
+export default withMDX(nextConfig);
