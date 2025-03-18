693539d12ba377f646ae90735cd66dc1cc49d00a
Sedat Uygur <sedat.uygur@outlook.com>
2025-03-19 02:20:06 +0300

    🎉 feat : add properties to Post type & fix some issues

--- components/mdx/PublishedOnOldBlog.tsx
+++ components/mdx/PublishedOnOldBlog.tsx
@@ -2,7 +2,7 @@ export function PublishedOnOldBlog() {
   return (
     <p className="italic">
       This post was originally published on my previous blog{" "}
-      <a href="https://sedat-can-uygur.vercel.app/blog" target="_blank">
+      <a href="#" target="_blank">
         sedat-can-uygur.vercel.app/blog
       </a>
     </p>
--- lib/types.d.ts
+++ lib/types.d.ts
@@ -2,6 +2,8 @@ export type Post = {
   content: string;
   date: string;
   description: string;
+  draft: boolean;
+  href?: string;
   slug: string;
   tags: string[];
   title: string;
