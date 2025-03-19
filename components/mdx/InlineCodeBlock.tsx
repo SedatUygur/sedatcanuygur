a0ee2362cb261de038853b99d1a61ea8b11352cb
Sedat Uygur <sedat.uygur@outlook.com>
2025-03-20 01:51:44 +0300

    🎉 feat : render an inline code block depending on the current theme

---
+++ components/mdx/InlineCodeBlock.tsx
@@ -0,0 +1,20 @@
+import { PropsWithChildren } from "react";
+
+/**
+ * Renders an inline code block with Solarized Light or Dark theme depending on
+ * the current theme.
+ *
+ * @param children the code block to render
+ * @returns the rendered code block
+ */
+
+export function InlineCodeBlock({ children }: PropsWithChildren) {
+  return (
+    <code
+      className="not-prose font-mono text-sm text-solaralizedlight-foreground bg-solaralizedlight
+      dark:text-solaralizeddark-foreground dark:bg-solaralizeddark rounded-md px-1.5 py-1"
+    >
+      {children}
+    </code>
+  );
+}
