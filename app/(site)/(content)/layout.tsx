335a98290f678a90c071d09262fb4eb99c4a056d
Sedat Uygur <sedat.uygur@outlook.com>
2025-03-19 19:23:01 +0300

    🎉 fix : fix layout for mobile

--- app/(site)/(content)/layout.tsx
+++ app/(site)/(content)/layout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export default function ContentLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="w-full flex justify-center">
+    <div className="md:w-full md:flex md:justify-center">
       <div className="prose dark:prose-invert">{children}</div>
     </div>
   );
