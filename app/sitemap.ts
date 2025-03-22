9de369eafefac0a5019a31a24a96303eede291dd
Sedat Uygur <sedat.uygur@outlook.com>
2025-03-22 23:31:09 +0300

    🎉 feat : update the styles

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from "next";
 
-import { fetchPosts } from "@/lib/FetchPosts";
+import { fetchMDXPosts } from "@/lib/FetchPosts";
 
 /**
  * Generates the sitemap for the blog.
@@ -8,7 +8,7 @@ import { fetchPosts } from "@/lib/FetchPosts";
  * @returns A promise that resolves with an array of objects containing the url and the last modified date for each route.
  */
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const posts = await fetchPosts();
+  const posts = await fetchMDXPosts();
 
   const blogs = posts.map((post) => ({
     url: `https://sedatcanuygur.vercel.app/blog/${post.slug}`,
