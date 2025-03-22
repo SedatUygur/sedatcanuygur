3205dda9ead325156f49ec3a6f841943930e22bf
Sedat Uygur <sedat.uygur@outlook.com>
2025-03-23 02:01:09 +0300

    🎉 feat : refactor header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,5 @@
-"use client";
-
 import Link from "next/link";
-import { usePathname } from "next/navigation";
-import * as React from "react";
-import { HomeIcon, BookOpen, Contact } from "lucide-react";
 
-import { GitHubButton } from "@/components/ui/GitHubButton";
 import { ModeToggle } from "@/components/ModeToggle";
 import {
   DropdownMenu,
@@ -13,46 +7,37 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/DropdownMenu";
+import { GitHubButton } from "@/components/ui/GitHubButton";
+import { LinkedInButton } from "@/components/ui/LinkedInButton";
+import { XButton } from "@/components/ui/XButton";
+import { HighlightedLink } from "@/components/ui/HighlightedLink";
 
 export function Header() {
-  const pathname = usePathname();
-
-  const baseIcon = "h-7 w-7";
-  const activeIcon = `${baseIcon} stroke-shine`;
   return (
-    <header className="mb-4">
-      <div className="hidden md:flex h-full flex-row">
+    <header className="mb-4 border-b border-primary dark:border-bright">
+      <div className="hidden md:flex h-full flex-row px-16">
         <div className="container px-0 flex flex-row items-center justify-between">
-          <div className="flex flex-row items-end space-x-4">
-            <Link aria-label="Home" href="/">
-              <HomeIcon
-                className={`link ${pathname === "/" ? activeIcon : baseIcon}`}
+          <HighlightedLink label="Home" href="/" />
+          <div className="flex flex-row items-center space-x-4">
+            <HighlightedLink label="About" href="/about" />
+            <HighlightedLink label="Blog" href="/blog" />
+            <HighlightedLink label="CV" href="/cv" />
+            <HighlightedLink label="Open Source" href="/open-source" />
+            <HighlightedLink label="Contact" href="/contact" />
+            <div className="flex flex-row items-center space-x-2">
+              <GitHubButton
+                href={"https://github.com/SedatUygur/sedatcanuygur"}
               />
-            </Link>
-            <Link aria-label="Blog" href="/blog">
-              <BookOpen
-                className={`link ${
-                  pathname.includes("/blog") ? activeIcon : baseIcon
-                }`}
+              <XButton href={"https://x.com/@_SedatUygur"} />
+              <LinkedInButton
+                href={"https://www.linkedin.com/in/sedat-can-uygur"}
               />
-            </Link>
-            <Link aria-label="Contact" href="/contact">
-              <Contact
-                className={`link ${
-                  pathname === "/contact" ? activeIcon : baseIcon
-                }`}
-              />
-            </Link>
-          </div>
-          <div>
-            <GitHubButton
-              href={"https://github.com/SedatUygur/sedatcanuygur"}
-            />
-            <ModeToggle />
+              <ModeToggle />
+            </div>
           </div>
         </div>
       </div>
-      <div className="md:hidden flex">
+      <div className="md:hidden flex px-16">
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
             <button className="text-gray-800 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400">
@@ -84,6 +69,11 @@ export function Header() {
             <DropdownMenuItem asChild>
               <Link aria-label="CV" href="/cv">
                 CV
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem asChild>
+              <Link aria-label="Open Source" href="/open-source">
+                Open Source
               </Link>
             </DropdownMenuItem>
             <DropdownMenuItem asChild>
