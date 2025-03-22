fb9ff83dcee650bbdc176e8562312af0146fec4d
Sedat Uygur <sedat.uygur@outlook.com>
2025-03-23 01:29:17 +0300

    🎉 feat : create a UI component for link highlight

---
+++ components/ui/HighlightedLink.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+type Props = {
+  href: string;
+  label: string;
+};
+
+export function HighlightedLink({ href, label }: Props) {
+  return (
+    <Link
+      className="text-primary hover:text-primary/70 dark:text-bright dark:hover:text-bright/70"
+      aria-label={label}
+      href={href}
+    >
+      {label}
+    </Link>
+  );
+}
