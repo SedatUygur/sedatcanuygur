c9192ac6727e03a754b975a1737b19b31c7fe4c4
Sedat Uygur <sedat.uygur@outlook.com>
2025-03-23 21:53:22 +0300

    🎉 feat : refactor color palette story

--- stories/colorPalette.stories.tsx
+++ stories/colorPalette.stories.tsx
@@ -1,60 +1,43 @@
 import * as React from "react";
 
-import type { Meta, StoryObj } from "@storybook/react";
-
-type ColorPaletteProps = {
-  name: string;
-  className: string;
-};
-// eslint-disable-next-line react/display-name
-const ColorPalette = React.forwardRef<HTMLDivElement, ColorPaletteProps>(
-  ({ ...props }, ref) => {
-    console.log("what are props?", props);
-    const classNameString = `h-20 w-20 ${props.className}`;
-    return (
-      <div ref={ref} className={classNameString}>
-        {props.name}
-      </div>
-    );
-  },
-);
+import type { Meta, StoryFn, StoryObj } from "@storybook/react";
 
 const meta = {
-  title: "ColorPalette",
-  component: ColorPalette,
-  parameters: {
-    layout: "centered",
-  },
+  component: React.Fragment,
+  parameters: {},
   tags: ["autodocs"],
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-} satisfies Meta<typeof ColorPalette>;
+  title: "ColorPalette",
+} satisfies Meta<React.ComponentProps<typeof React.Fragment>>;
 
-export default meta;
-type Story = StoryObj<typeof meta>;
-
-// Don't quite understand what storybook is yelling about. Also the backgrounds aren't working so whatever
-
-export const Primary: Story = {
-  args: {
-    name: "Primary Color",
-    className: "bg-primary",
-  },
-  render: () => (
-    <div className="flex flex-col">
-      <div className="flex flex-row">
-        <ColorPalette name="Midnight" className="bg-midnight" />
-        <ColorPalette name="Twilight" className="bg-twilight" />
-        <ColorPalette name="Dusk" className="bg-dusk" />
-        <ColorPalette name="Cloud" className="bg-cloud" />
-        <ColorPalette name="Fog" className="bg-fog" />
-      </div>
-      <div className="flex flex-row">
-        <ColorPalette name="Radiance" className="bg-radiance" />
-        <ColorPalette name="Warmth" className="bg-warmth" />
-        <ColorPalette name="Shine" className="bg-shine" />
-        <ColorPalette name="Gleam" className="bg-gleam" />
-        <ColorPalette name="Bright" className="bg-bright" />
+const Template: StoryFn<React.ComponentProps<typeof React.Fragment>> = () => (
+  <React.Fragment>
+    <div className="flex flex-row w-full space-x-4">
+      <div className="flex">
+        <div className="flex flex-col">
+          <div className="flex flex-row">
+            <div className="h-20 w-20 bg-midnight">Midnight</div>
+            <div className="h-20 w-20 bg-twilight">Twilight</div>
+            <div className="h-20 w-20 bg-dusk">Dusk</div>
+            <div className="h-20 w-20 bg-cloud">Cloud</div>
+            <div className="h-20 w-20 bg-fog">Fog</div>
+          </div>
+          <div className="flex flex-row">
+            <div className="h-20 w-20 bg-radiance">Radiance</div>
+            <div className="h-20 w-20 bg-warmth">Warmth</div>
+            <div className="h-20 w-20 bg-shine">Shine</div>
+            <div className="h-20 w-20 bg-gleam">Gleam</div>
+            <div className="h-20 w-20 bg-bright">Bright</div>
+          </div>
+        </div>
       </div>
     </div>
-  ),
+  </React.Fragment>
+);
+
+type Story = StoryObj<typeof meta>;
+
+export default meta;
+
+export const Primary: Story = {
+  render: Template,
 };
