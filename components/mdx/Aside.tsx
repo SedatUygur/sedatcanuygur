e15de3e3766d15cc8d9ce8e78663122eeaf1ad2f
Sedat Uygur <sedat.uygur@outlook.com>
2025-03-23 20:40:24 +0300

    🎉 feat : create an aside component

---
+++ components/mdx/Aside.tsx
@@ -0,0 +1,12 @@
+type Props = {
+  heading: string;
+} & React.PropsWithChildren;
+
+export function Aside({ heading, children }: Props) {
+  return (
+    <aside className="p-4 border-l-4 border-l-blue-400 rounded-xl bg-gray-100 dark:bg-dusk">
+      <h6 className="font-bold">{heading}</h6>
+      {children}
+    </aside>
+  );
+}
